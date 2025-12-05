69d6cb83f9fa8f8fc5520dee78e20a45ff2a0aa5
Aisha Shaheed <ps.ashaheed@outlook.com>
2025-12-04 20:30:08 -0500

    Updated me answers

--- script.js
+++ script.js
@@ -34,24 +34,24 @@
 // If a number is not divisible by either 3 or 5, log the number.
 
 
-for (i=1; i<=100; i++){
-    if (i % 3 !== 0 || i % 5 !== 0){
-
-    }
-    console.log(i);
-}
+ for (i=1; i<=100; i++){
+     if (i % 3 !== 0 && i % 5 !== 0){
+console.log(i);
+     }
+     
+ }
 
 
 
 // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 
 
-for (i=1; i<=100; i++){
-    if (i % 3 !== 0 && i % 5 !== 0){
-
-    }
-    console.log(i);
-}
+// for (i=1; i<=100; i++){
+    // if (i % 3===0 && i % 5===0){
+//  console.log('Fizz Buzz');
+    // }
+   
+//  }
 
 
 
