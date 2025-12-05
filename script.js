8eaee7da76493a33613c053fe631c1ee02449784
Aisha Shaheed <ps.ashaheed@outlook.com>
2025-12-05 10:55:21 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -53,6 +53,56 @@ console.log(i);
    
 //  }
 
+// Part 3: Feeling Loopy
+// As a final task, solve the following practical problem regarding string processing.
+// Context: A CSV file, or “Comma-Separated Values” file is traditionally used to store tabular data. You may be familiar with 
+// CSVs through past use of programs such as Microsoft Excel or Google Sheets. While each of these programs save their data in 
+// different formats to preserve style (e.g., font color or cell backgrounds), at their core, they are storing CSV data.
+
+let str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor\'s Assistant,26;"
+let newLine = "";//empty string to receive new data without '\n'
+
+//The for...in loop will iterate over the string indices as strings.
+
+for (const i in str){   // i is included to play a role in the number of iterations.
+const currentIndex = num(i);   //Converting each string in the index into numbers.
+const char = str[currentIndex];
+}
+//Finally....
+    if(char === '\n' || currentIndex=== str.length--){  //(it is .length-1) as it iterates and goes through each cell the 
+    // data is removed.
+    // If we are at the last character, we must append it before printing.
+        if (currentIndex === str.length --) {
+            newLine += char;
+        }
+//for (let index = 0; index < array.length; index++) {
+  //  const element = array[index];
+    console.log(newLine);
+    
+}    
+else {
+        // If it's a regular character (not a newLine), append it to the current line.
+        newLine += char;
+    }
+
+//for (char in str) {
+	//if (str[char] == '\n') {
+	//	continue;
+	//}
+
+	//console.log(str);
+//}
+
+//After further research, I came up with the formula below:
+
+
+
+
+
+
+
+
+
 
 
 
